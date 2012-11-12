7cf78a67e2cf1734298e11bf941fdd58d8dcde06
Erik Rozendaal <erik@deler.org>
2012-11-12 15:45:43 +0100

    Fetcher related unit tests.

--- rpki-validator-cli/src/test/java/net/ripe/certification/validator/fetchers/RsyncRpkiRepositoryObjectFetcherTest.java
+++ rpki-validator-cli/src/test/java/net/ripe/certification/validator/fetchers/RsyncRpkiRepositoryObjectFetcherTest.java
@@ -35,11 +35,9 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import net.ripe.certification.validator.RepositoryObjectsSetUpHelper;
 import net.ripe.certification.validator.commands.TopDownWalkerTest;
 import net.ripe.certification.validator.util.UriToFileMapper;
-import net.ripe.commons.certification.cms.manifest.ManifestCms;
-import net.ripe.commons.certification.crl.X509Crl;
+import net.ripe.commons.certification.CertificateRepositoryObject;
 import net.ripe.commons.certification.rsync.Rsync;
 import net.ripe.commons.certification.util.Specifications;
 import net.ripe.commons.certification.validation.ValidationCheck;
@@ -49,11 +47,9 @@ import net.ripe.commons.certification.validation.ValidationStatus;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
-public class RsyncCertificateRepositoryObjectFetcherTest {
+public class RsyncRpkiRepositoryObjectFetcherTest {
 
     private static final File TEST_TARGET_DIRECTORY = new File(System.getProperty("java.io.tmpdir", "/tmp"));
 
@@ -61,22 +57,18 @@ public class RsyncCertificateRepositoryObjectFetcherTest {
 
     private static final File TEST_REPOSITORY_DIRECTORY = new File(TEST_TARGET_DIRECTORY, "localhost:9999/repo/ca%20repo/");
 
-    private static final URI VALIDATION_URI = TEST_REPOSITORY_URI.resolve("validated-object");
+    private static final URI VALIDATION_URI = TEST_REPOSITORY_URI.resolve("object.cer");
 
     private static final URI TEST_OBJECT_CERT_URI = TEST_REPOSITORY_URI.resolve("object.cer");
     private static final File TEST_OBJECT_CERT_FILE = new File(TEST_TARGET_DIRECTORY, "localhost:9999/repo/ca%20repo/object.cer");
-
-    private static final URI TEST_OBJECT_MFT_URI = TEST_REPOSITORY_URI.resolve("object.mft");
-
 
     private boolean rsyncExecuted = false;
     private int rsyncExitCode = 0;
     private byte[] rsyncFileContents;
     private Rsync rsync;
-    private ManifestCms manifest;
-    private X509Crl crl;
+    private CertificateRepositoryObject object;
     private ValidationResult validationResult;
-    private CertificateRepositoryObjectFetcher subject;
+    private RpkiRepositoryObjectFetcher subject;
 
     @Before
     public void setUp() {
@@ -94,11 +86,10 @@ public class RsyncCertificateRepositoryObjectFetcherTest {
                 return rsyncExitCode;
             }
         };
-        manifest = TopDownWalkerTest.getRootManifestCms();
-        crl = RepositoryObjectsSetUpHelper.getRootCrl();
+        object = TopDownWalkerTest.getRootManifestCms();
         validationResult = new ValidationResult();
         validationResult.setLocation(new ValidationLocation(VALIDATION_URI));
-        subject = new RpkiRepositoryObjectFetcherAdapter(new RsyncRpkiRepositoryObjectFetcher(rsync, new UriToFileMapper(TEST_TARGET_DIRECTORY)));
+        subject = new RsyncRpkiRepositoryObjectFetcher(rsync, new UriToFileMapper(TEST_TARGET_DIRECTORY));
     }
 
     @After
@@ -108,28 +99,28 @@ public class RsyncCertificateRepositoryObjectFetcherTest {
 
     @Test
     public void shouldFetchObject() {
-        rsyncFileContents = manifest.getEncoded();
-        assertEquals(manifest, subject.getObject(TEST_OBJECT_MFT_URI, null, Specifications.<byte[]>alwaysTrue(), validationResult));
+        rsyncFileContents = object.getEncoded();
+        assertEquals(object, subject.fetch(TEST_OBJECT_CERT_URI, Specifications.<byte[]>alwaysTrue(), validationResult));
         assertEquals(new ValidationCheck(ValidationStatus.PASSED, KNOWN_OBJECT_TYPE, VALIDATION_URI.toString()), validationResult.getResult(new ValidationLocation(VALIDATION_URI), KNOWN_OBJECT_TYPE));
     }
 
     @Test
     public void shouldNotFetchObjectIfContentsCannotBeVerified() {
-        rsyncFileContents = manifest.getEncoded();
-        assertNull("content verification must fail", subject.getObject(TEST_OBJECT_CERT_URI, null, Specifications.<byte[]>alwaysFalse(), validationResult));
+        rsyncFileContents = object.getEncoded();
+        assertNull("content verification must fail", subject.fetch(TEST_OBJECT_CERT_URI, Specifications.<byte[]>alwaysFalse(), validationResult));
         assertEquals(new ValidationCheck(ValidationStatus.ERROR, VALIDATOR_FILE_CONTENT, TEST_OBJECT_CERT_URI.toString()), validationResult.getResult(new ValidationLocation(TEST_OBJECT_CERT_URI), VALIDATOR_FILE_CONTENT));
     }
 
     @Test
     public void shouldNotFetchObjectIfContentsCannotBeParsed() {
         rsyncFileContents = new byte[] { 0x10, 0x12, 0x3 };
-        assertNull("content should not be parsed", subject.getObject(TEST_OBJECT_CERT_URI, null, Specifications.<byte[]>alwaysTrue(), validationResult));
+        assertNull("content should not be parsed", subject.fetch(TEST_OBJECT_CERT_URI, Specifications.<byte[]>alwaysTrue(), validationResult));
         assertEquals(new ValidationCheck(ValidationStatus.ERROR, KNOWN_OBJECT_TYPE, TEST_OBJECT_CERT_URI.toString()), validationResult.getResult(new ValidationLocation(TEST_OBJECT_CERT_URI), KNOWN_OBJECT_TYPE));
     }
 
     @Test
     public void shouldFetchObjectUsingRsync() {
-        subject.getObject(TEST_OBJECT_CERT_URI, null, Specifications.<byte[]>alwaysTrue(), validationResult);
+        subject.fetch(TEST_OBJECT_CERT_URI, Specifications.<byte[]>alwaysTrue(), validationResult);
 
         assertTrue("rsync executed", rsyncExecuted);
         assertFalse("rsync --recursive must not be added for single file", rsync.containsOption("--recursive"));
@@ -143,7 +134,7 @@ public class RsyncCertificateRepositoryObjectFetcherTest {
         assertTrue("repository prefetched", rsyncExecuted);
 
         rsyncExecuted = false;
-        subject.getObject(TEST_OBJECT_CERT_URI, null, Specifications.<byte[]>alwaysTrue(), validationResult);
+        subject.fetch(TEST_OBJECT_CERT_URI, Specifications.<byte[]>alwaysTrue(), validationResult);
         assertFalse("rsync should not execute for cached uri", rsyncExecuted);
     }
 
@@ -151,56 +142,14 @@ public class RsyncCertificateRepositoryObjectFetcherTest {
     public void shouldNotFetchObjectIfFileCannotBeRead() {
         rsyncExitCode = 0;
         rsyncFileContents = null;
-        assertNull(subject.getObject(TEST_OBJECT_CERT_URI, null, Specifications.<byte[]>alwaysFalse(), validationResult));
+        assertNull(subject.fetch(TEST_OBJECT_CERT_URI, Specifications.<byte[]>alwaysFalse(), validationResult));
         assertFalse(validationResult.getResult(new ValidationLocation(TEST_OBJECT_CERT_URI), VALIDATOR_READ_FILE).isOk());
     }
 
     @Test
-    public void shouldFetchManifest() {
-        rsyncFileContents = manifest.getEncoded();
-        assertEquals(manifest, subject.getManifest(TEST_OBJECT_CERT_URI, null, validationResult));
-    }
-
-    @Test
-    public void shouldNotFetchManifestIfObjectNotFound() {
-        rsyncExitCode = 1;
-        rsyncFileContents = null;
-        assertNull(subject.getManifest(TEST_OBJECT_CERT_URI, null, validationResult));
-        assertTrue(validationResult.hasFailureForCurrentLocation());
-    }
-
-    @Test
-    public void shouldFetchManifestAndFailIfObjectIsNotManifest() {
-        rsyncFileContents = crl.getEncoded();
-        assertNull(subject.getManifest(TEST_OBJECT_CERT_URI, null, validationResult));
-        assertTrue(validationResult.hasFailureForCurrentLocation());
-    }
-
-    @Test
-    public void shouldFetchCrl() {
-        rsyncFileContents = crl.getEncoded();
-        assertEquals(crl, subject.getCrl(TEST_OBJECT_CERT_URI, null, validationResult));
-    }
-
-    @Test
-    public void shouldNotFetchCrlIfObjectNotFound() {
-        rsyncExitCode = 1;
-        rsyncFileContents = null;
-        assertNull(subject.getCrl(TEST_OBJECT_CERT_URI, null, validationResult));
-        assertTrue(validationResult.hasFailureForCurrentLocation());
-    }
-
-    @Test
-    public void shouldFetchCrlAndFailIfObjectIsNotCrl() {
-        rsyncFileContents = manifest.getEncoded();
-        assertNull(subject.getCrl(TEST_OBJECT_CERT_URI, null, validationResult));
-        assertTrue(validationResult.hasFailureForCurrentLocation());
-    }
-
-    @Test
     public void shouldNotFetchObjectIfUriCannotBeMapped() {
-        rsyncFileContents = manifest.getEncoded();
-        assertNull(subject.getObject(URI.create("rsync:///missinghost/"), null, Specifications.<byte[]>alwaysTrue(), validationResult));
+        rsyncFileContents = object.getEncoded();
+        assertNull(subject.fetch(URI.create("rsync:///missinghost/"), Specifications.<byte[]>alwaysTrue(), validationResult));
         assertTrue(validationResult.hasFailureForCurrentLocation());
     }
 
