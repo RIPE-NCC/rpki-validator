bcb5a3dbdb0a239b9e63e5f2316309d24affc389
Andrew Snare <asnare@xebia.com>
2014-03-05 13:23:30 +0100

    Fix compilation error; the java collection is raw.
    Previous code seems to have picked up a version (via a transitive dependency) which returned a non-raw collection.

--- rpki-validator-app/src/main/scala/net/ripe/rpki/validator/config/Main.scala
+++ rpki-validator-app/src/main/scala/net/ripe/rpki/validator/config/Main.scala
@@ -59,6 +59,7 @@ import net.ripe.rpki.validator.models.IgnoreFilter
 import net.ripe.rpki.validator.api.RestApi
 import com.codahale.metrics.servlets.{HealthCheckServlet, MetricsServlet}
 import net.ripe.rpki.validator.config.health.HealthChecks
+import java.io.File
 
 object Main {
   private val sessionId: Pdu.SessionId = Pdu.randomSessionid()
@@ -128,9 +129,10 @@ class Main() { main =>
   actorSystem.scheduler.schedule(initialDelay = 0.seconds, interval = 2.hours) { refreshRisDumps() }
 
   private def loadTrustAnchors(): TrustAnchors = {
-    import java.{ util => ju }
-    val tals = new ju.ArrayList(FileUtils.listFiles(ApplicationOptions.talDirLocation, Array("tal"), false))
-    TrustAnchors.load(tals.asScala)
+    import java.util.{Collection => jCollection}
+    val tals = FileUtils.listFiles(ApplicationOptions.talDirLocation, Array("tal"), false)
+        .asInstanceOf[jCollection[File]]
+    TrustAnchors.load(tals.asScala.toSeq)
   }
 
   private def refreshRisDumps() {
