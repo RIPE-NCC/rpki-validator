681c8c64000f6f3133bc6f4ec4ad32676822339e
Thiago da Cruz <tdacruzper@ripe.net>
2015-01-30 16:45:11 +0100

    add missing method to the storage stub and license text.

--- rpki-validator-app/src/test/scala/net/ripe/rpki/validator/models/TopDownWalkerSpec.scala
+++ rpki-validator-app/src/test/scala/net/ripe/rpki/validator/models/TopDownWalkerSpec.scala
@@ -6,14 +6,14 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * - Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * - Neither the name of the RIPE NCC nor the names of its contributors may be
- * used to endorse or promote products derived from this software without
- * specific prior written permission.
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the RIPE NCC nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software without
+ *     specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -260,6 +260,8 @@ class TopDownWalkerSpec extends ValidatorTestCase with BeforeAndAfterEach {
       override def getBroken: Seq[BrokenObject] = ???
 
       override def getCertificate(uri: String): Option[CertificateObject] = ???
+
+      override def delete(url: String, hash: String) = ???
 
     }
   }
