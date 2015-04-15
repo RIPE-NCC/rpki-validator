359450054c93e77ce1ea0e54c4cddaca6777dd1a
Misja Alma <malma@xebia.com>
2015-04-15 16:26:02 +0200

    progress with the rpkiTreeBuilder

--- rpki-validator-app/src/test/scala/net/ripe/rpki/validator/models/RpkiTreeBuilder.scala
+++ rpki-validator-app/src/test/scala/net/ripe/rpki/validator/models/RpkiTreeBuilder.scala
@@ -1,3 +1,32 @@
+/**
+ * The BSD License
+ *
+ * Copyright (c) 2010-2012 RIPE NCC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the RIPE NCC nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.ripe.rpki.validator.models
 
 import java.math.BigInteger
@@ -47,6 +76,16 @@ class RpkiTreeBuilder(store: Storage) {
 
   def build = {
     // TODO Build and store the whole tree, starting with the rootCert
+    val rootCert = buildRootCertificate(rootCertName, rootKeyPair)
+
+    buildTreeFrom(rootCertName)
+  }
+
+  def buildTreeFrom(certificateName: X500Principal) = {
+    val mft = buildMft(certificateName)
+    val crl = buildCrl(certificateName)
+    // TODO store them
+    // TODO also build other objects and recurse on certs
   }
 
   private def buildRootCertificate(name: X500Principal, keyPair: KeyPair) = {
