557b2db149974b783790a576e47eb4c7da79c9a8
Varesh Tapadia <vtapadia@ripe.net>
2015-11-20 11:53:14 +0100

    Adding docker specific configuration

--- rpki-validator-app/docker/docker-startup.sh
+++ rpki-validator-app/docker/docker-startup.sh
@@ -31,4 +31,4 @@
 
 rm -f /opt/docker/rpki-validator-app-*/conf/tal/*.tal
 cp /opt/docker/conf/tal/* /opt/docker/rpki-validator-app-*/conf/tal/
-/opt/docker/rpki-validator-app-*/rpki-validator.sh run
+/opt/docker/rpki-validator-app-*/rpki-validator.sh run -c /opt/docker/conf/rpki-validator-docker.conf
