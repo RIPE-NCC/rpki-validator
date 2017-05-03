5d07b06aa81383b94ebc066a68d7b2f0c97b7b8c
Mikhail Puzanov <mpuzanov@ripe.net>
2017-05-03 10:49:35 +0200

    WIP: add more logic to health checks.

--- rpki-validator-app/src/main/scala/net/ripe/rpki/validator/config/health/HealthServlet.scala
+++ rpki-validator-app/src/main/scala/net/ripe/rpki/validator/config/health/HealthServlet.scala
@@ -27,42 +27,15 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-/**
-  * The BSD License
-  *
-  * Copyright (c) 2010-2012 RIPE NCC
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are met:
-  *   - Redistributions of source code must retain the above copyright notice,
-  * this list of conditions and the following disclaimer.
-  *   - Redistributions in binary form must reproduce the above copyright notice,
-  * this list of conditions and the following disclaimer in the documentation
-  * and/or other materials provided with the distribution.
-  *   - Neither the name of the RIPE NCC nor the names of its contributors may be
-  * used to endorse or promote products derived from this software without
-  * specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-  * POSSIBILITY OF SUCH DAMAGE.
-  */
 package net.ripe.rpki.validator.config.health
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 import net.ripe.rpki.commons.rsync.Rsync
 import net.ripe.rpki.commons.validation.ValidationStatus
+import net.ripe.rpki.validator.config.ApplicationOptions
 import net.ripe.rpki.validator.models.{TrustAnchors, ValidatedObjects}
+import org.joda.time.Instant
 
 object Code extends Enumeration {
   type Code = Value
@@ -92,7 +65,7 @@ abstract class HealthServlet extends HttpServlet {
 
   protected def getTrustAnchors: TrustAnchors
 
-  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
+  override def doGet(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
 
     val statuses = taStatuses ++ Map(
       "rsync" -> rsyncHealthCheck,
@@ -145,7 +118,19 @@ abstract class HealthServlet extends HttpServlet {
   }
 
 
-  private def lastValidationTimeCheck = Status.ok
+  private def lastValidationTimeCheck = {
+    val lastUpdated = getTrustAnchors.all.filter(_.enabled).map(_.lastUpdated)
+    val interval = ApplicationOptions.validationInterval.length
+    val now = Instant.now
+    lastUpdated.exists(_.exists(_.isBefore(now.minus(interval))))
+    if (lastUpdated.exists(_.isEmpty))
+      Status.warning("Not all TA's are validated.")
+    else if (lastUpdated.exists(_.exists(_.isBefore(now.minus(interval)))))
+      Status.error("Some ")
+    else
+      Status.ok
+  }
+
   private def jvmMemoryCheck = Status.ok
 
 }
